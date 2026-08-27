4a14f9c40413da662da430804eef9c5595b00d92
josiah-byte08 <josiahmmjackson@gmail.com>
2026-08-26 19:17:56 -0500

    Add files via upload

---
+++ app/ClassContext.tsx
@@ -0,0 +1,28 @@
+import {useState, useContext, createContext } from "react";
+import { useClasses }
+type Class = {
+className: string;
+professor: string;
+room: string;
+startTime: string;
+endTime: string;
+selectedDays: number[];
+}
+
+const ClassContext = createContext(null);
+
+export function ClassProvider({ children }) {
+	const [classes, setClasses] = useState<Class[]>([]);
+
+	return (
+		<ClassContext.Provider value= {{ classes, setClasses}}>
+		{children}
+		</ClassContext.Provider>
+	);
+}
+
+export function useClasses(){
+	const context = useContext(ClassContext);
+
+	return context;
+}
